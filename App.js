6588b8495f3b28e72475ef0f17b2e5fa98da8816
William du Toit <Williamdutoitdev@gmail.com>
2022-04-30 12:19:14 +0200

    Initial commit2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: "#ffff"}}>Objective Calvary</Text>
+      <Text style={{color: "#ffff"}}>Objective Calvary!</Text>
       <StatusBar style="auto" />
     </View>
   );
